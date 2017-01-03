f4165fc483e0678578a5dd6fe281950ffa4b542d
Raju Surarapu <rajusurarapu16@gmail.com>
2017-01-02 23:26:27 -0800

    Signed-off-by: Raju Surarapu <rajusurarapu16@gmail.com>

---
+++ ava8/src/ava8/HelloWord.java
@@ -0,0 +1,13 @@
+package ava8;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// gettings
+		// dafadsfdsfs
+      System.out.println("Hello World sdasdddas");
+      
+	}
+
+}
